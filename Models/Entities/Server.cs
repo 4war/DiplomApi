a154058f69ed04b55e209e777359821f2d465293
4war <fedro_777@mail.ru>
2022-01-28 12:08:20 +0400

    First Commit of Api

---
+++ Models/Entities/Server.cs
@@ -0,0 +1,9 @@
+﻿namespace Advantage.API.Models.Entities
+{
+    public class Server
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsOnline { get; set; }
+    }
+}
